8e0db198efeabc3c91df09627382f0d4b6da7865
Freakruit <freakruit@gmail.com>
2024-01-21 17:41:31 +0530

    v3

--- header.php
+++ header.php
@@ -16,16 +16,16 @@
 <link href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 <!-- Stylesheets -->
-<link href="font-awesome-all.css" rel="stylesheet">
-<link href="flaticon.css" rel="stylesheet">
-<link href="owl.css" rel="stylesheet">
-<link href="bootstrap.css" rel="stylesheet">
-<link href="jquery.fancybox.min.css" rel="stylesheet">
-<link href="animate.css" rel="stylesheet">
-<link href="nice-select.css" rel="stylesheet">
-<link href="color.css" rel="stylesheet">
-<link href="style.css" rel="stylesheet">
-<link href="responsive.css" rel="stylesheet">
-<link href="custom.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="font-awesome-all.css" >
+<link rel="stylesheet" type="text/css" href="flaticon.css">
+<link rel="stylesheet" type="text/css" href="owl.css">
+<link rel="stylesheet" type="text/css" href="bootstrap.css">
+<link rel="stylesheet" type="text/css" href="jquery.fancybox.min.css">
+<link rel="stylesheet" type="text/css" href="animate.css">
+<link rel="stylesheet" type="text/css" href="nice-select.css">
+<link rel="stylesheet" type="text/css" href="color.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="responsive.css">
+<link rel="stylesheet" type="text/css" href="custom.css">
 
 </head>
